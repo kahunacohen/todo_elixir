652e70a24365d2bb6680d59648135a6ba53605c9
Aharon Cohen <aharonc@matav.org.il>
2024-07-25 17:44:49 +0300

    added delete

--- lib/todolist.ex
+++ lib/todolist.ex
@@ -33,6 +33,22 @@ defmodule Todolist do
     |> Enum.filter(fn entry -> entry.date == date end)
   end
 
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %Todolist{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %Todolist{todo_list | entries: new_entries}
+  end
+
   def start() do
     todos =
       Todolist.new()
@@ -40,7 +56,24 @@ defmodule Todolist do
       |> Todolist.add_entry(%Todo{date: ~D[2023-12-11], title: "Supermarket"})
       |> Todolist.add_entry(%Todo{date: ~D[2023-12-11], title: "Work"})
       |> Todolist.add_entry(%Todo{date: ~D[2024-06-26], title: "Bills"})
+    IO.puts("initialized todos:")
+    IO.inspect(todos)
+    IO.puts("\n")
 
-    Todolist.entries(todos, ~D[2023-12-11])
+    IO.puts("filtered todos:")
+    IO.inspect(Todolist.entries(todos, ~D[2023-12-11]))
+    IO.puts("\n")
+
+    IO.puts("updated entry:")
+    IO.inspect(update_entry(todos, 0, &Map.put(&1, :date, ~D[2023-12-20])))
+    IO.puts("\n")
+
+    IO.puts("deleted entry:")
+    IO.inspect(delete_entry(todos, 2))
+    IO.puts("\n")
+
+    IO.puts("see, original items weren't affected")
+    todos
+
   end
 end
